cca73f025495a46db5253754bad672273eb40b7a
juncode37 <juncode37@gmail.com>
2026-01-16 12:29:40 +0200

    Updates

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+// validation
+(function () {
+        "use strict";
+        var forms = document.querySelectorAll(".needs-validation");
+        Array.prototype.slice.call(forms).forEach(function (form) {
+          form.addEventListener(
+            "submit",
+            function (event) {
+              if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add("was-validated");
+            },
+            false
+          );
+        });
+      })();
+
+// vin transform
+document.querySelector('.header__input').addEventListener('input', function(e) {
+  e.target.value = e.target.value.toUpperCase();
+});
